d59c8cd415b334a974dc52463552c6b4ca2452f4
gkevink <gkangkai@gmail.com>
2016-09-04 13:22:44 +0800

    framework: use flask

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from flask import Flask, session, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    app.run()
+
