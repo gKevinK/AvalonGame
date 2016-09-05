381f7e6fad326399f7a071b19baa3ec5ab2205cb
Kevin <gkangkai@gmail.com>
2016-09-05 14:18:35 +0800

    templates: add some templates

---
+++ app/machine.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class AvalonMachine(object):
+    
+    def AvalonMachine(num, **args):
+        pass
+
+    def get_roles():
+        pass
+    
+    def vote(player, agree):
+        pass
+
+    def success(player, success):
+        pass
+
+    def assassin(target):
+        pass
+
+class Machine(object):
+    pass
--- app/main.py
+++ app/main.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, session, render_template, json
+from flask import Flask, session, g, render_template, json
+
+from machine import Machine
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello world!"
+    return render_template('index.html')
+
+@app.route('/set_profile', methods=['Post'])
+def set_profile():
+    pass
+
+@app.route('/start_new')
+def start_new():
+    pass
+
+@app.route('/join')
+def join():
+    pass
+
+
 
 if __name__ == '__main__':
     app.run()
